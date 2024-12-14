192c8363d6e3a75b22ecfd5c7dd9b3b8a2327149
unknown <carolinehoeg123@gmail.com>
2024-12-14 13:42:58 +0100

    DDD cont.

---
+++ Cart.Domain/Exceptions/NegativeOrZeroAmountException.cs
@@ -0,0 +1,7 @@
+﻿namespace Cart.Domain.Exceptions
+{
+    public class NegativeOrZeroAmountException : Exception
+    {
+        public NegativeOrZeroAmountException(string message) : base(message) { }
+    }
+}
